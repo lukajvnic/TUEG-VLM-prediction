55341b9fb2d0f858182626c9a48d59e6bbe2c1eb
Luka Jovanovic <luka@narval1.narval.calcul.quebec>
2026-07-01 08:52:50 -0400

    First GPU run

---
+++ RUNGUIDE.md
@@ -0,0 +1,5 @@
+# RUNGUIDE
+
+Once the repository is set up, this is a simple guide on how to properly run this script in Narval.
+
+##
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=ollama-vlm
+#SBATCH --account=def-milad777
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --time=1:00:00
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+cd $SCRATCH/TUEG-VLM-prediction
+
+module load apptainer
+
+# Activate Python environment
+source .venv/bin/activate
+
+# W&B offline because GPU nodes may not have internet
+export WANDB_MODE=offline
+
+# Tell Ollama where the downloaded model files are
+export OLLAMA_MODELS=$SCRATCH/ollama/models
+
+# Start Ollama server in background
+apptainer exec --nv $SCRATCH/ollama/ollama.sif ollama serve > $SCRATCH/ollama/server.log 2>&1 &
+
+sleep 15
+
+# Run script
+python eval.py
+
+# Clean up server
+pkill -f "ollama serve"
