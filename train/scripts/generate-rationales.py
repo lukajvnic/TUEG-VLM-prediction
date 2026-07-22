0dd3543ade63e9b6f021ece2bd91d597ef48ab65
lukajvnic <lukaj2501@gmail.com>
2026-07-22 16:42:08 -0400

    began writing train script

---
+++ train/scripts/generate-rationales.py
@@ -0,0 +1,9 @@
+'''
+This program takes one LLM model, provides the image and a prompt containing the ground truth answer, and tells it to create a text rationale.
+It will save it to rationales.csv (?) under each of the datasets (e.g. datasets/TUEP/rationales.csv)
+this will just be a copy of labels.csv with an additional column "rationale" that is generated based on the prior information.
+'''
+
+
+def main():
+    pass
